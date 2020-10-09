58b6e807423b570ed220c639fcbe39b0e1acbe63
Guilherme <guilherme.viegas@tecnico.ulisboa.pt>
2020-10-09 15:30:26 +0100

    Ridge done

--- Lab1/teste1.py
+++ Lab1/teste1.py
@@ -78,18 +78,43 @@ plt.plot(array_x, array_y, 'b*')
 plt.show()
 
 error = y_calc - array_y    
-sse = np.matmul(np.transpose(error), error)
+print(np.matmul(np.transpose(error), error))
+# dont know exactly what are outliers and if they need to be removed à la pata
 
-"""
 # %% Regularization
 from sklearn import linear_model
+import numpy as np
+import matplotlib.pyplot as plt
 
 data3_x = np.load('data3_x.npy')
 data3_y = np.load('data3_y.npy')
+alpha_arr = np.arange(0.001, 10, 0.01)
+coefs = []
 
-reg = linear_model.RidgeCV(alphas=np.logspace(-3, 1, 5), 10000)
-reg.fit(data3_x, data3_y)
-"""
+beta, X = LSestimation(len(data3_x), 2, data3_x, data3_y)
+beta_aux = np.tile(np.transpose(beta),(len(alpha_arr),1))
+
+for a in alpha_arr:
+    ridge = linear_model.Ridge(alpha=a, max_iter= 10000)
+    ridge.fit(data3_x, data3_y)
+    if a == 0.001:
+        coefs = ridge.coef_
+    else:
+        coefs = np.r_[coefs, ridge.coef_]
+
+# DUVIDA: Usa-se B0, B1 e B2 ou B1,B2,B3
+plt.figure()
+plt.plot(alpha_arr, beta_aux, linestyle='--')
+plt.plot(alpha_arr, coefs)
+plt.legend(['B1 LS', 'B2 LS', 'B3 LS', 'B1 Ridge', 'B2 Ridge', 'B3 Ridge'])
+plt.xscale('log')
+plt.xlabel('Alpha')
+plt.ylabel('Coefficients')
+plt.title('Regularization - Ridge Method')
+plt.axis('tight')
+plt.show()
+
+
 
 
 
